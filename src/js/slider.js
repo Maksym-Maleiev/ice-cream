efd7e8049def29ae2358f2700727bedb631114c7
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-10-13 10:04:36 +0300

    Update slider.js

--- src/js/slider.js
+++ src/js/slider.js
@@ -8,24 +8,5 @@ new Swiper(".image-slider", {
     clickable: true,
     // Dynamic bullets
     dynamicBullets: true,
-    // Custom bullets
-    renderBullet: function (index, className) {
-      return '<span class="' + className + '">' + (index + 1) + '</span>';
-    },
-
-    // Fraction
-    type: "fraction",
-    // Custom fraction
-    renderFraction: function (currentClass, totalClass) {
-      return (
-        'Photo <span class="' +
-        currentClass +
-        '"></span>' +
-        " from " +
-        '<span class="' +
-        totalClass +
-        '"></span>'
-      );
-    },
   },
 });
