9e8905d219bd2752900dfe228c4246d468e38bc1
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-10-13 09:38:49 +0300

    Update slider.js

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,5 +1,5 @@
 // Install swiper-slider
-new Swiper(".image-slider"), {
+new Swiper(".image-slider", {
   // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
@@ -9,8 +9,14 @@ new Swiper(".image-slider"), {
   // Pagination
   pagination: {
     el: ".swiper-pagination",
-     // Bullets
-      type: 'bullets',
-      clickable: true,
+    // Bullets
+    type: 'bullets',
+    clickable: true,
+    // Dynamic bullets
+    dynamicBullets: true,
+    // Custom bullets
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + '</span>';
+    }
   },
-}
+});
