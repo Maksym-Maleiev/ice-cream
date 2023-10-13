6c0c51b5b2f24f2d1936b65a947112edc3dc342a
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-10-13 09:51:36 +0300

    Update slider.js

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,11 +1,5 @@
 // Install swiper-slider
 new Swiper(".image-slider", {
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: '.swiper-button-prev',
-  },
-
   // Pagination
   pagination: {
     el: ".swiper-pagination",
